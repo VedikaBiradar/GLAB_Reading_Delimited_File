02151b0bc1e20b77a695e5de6d925a753a6860b1
VedikaBiradar <vedikabiradar2001@gmail.com>
2024-05-06 16:43:18 -0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,13 @@
+import java.io.FileNotFoundException;
+
+public class Main {
+    public static void main(String[] args) throws Exception, FileNotFoundException {
+
+//        ScanDelimiterdFile scanDelimiter= new ScanDelimiterdFile();
+//        scanDelimiter.scanDelimeter();
+
+        MyRunner mr = new MyRunner();
+        mr.scandelimetereUsingobject();
+
+    }
+}
